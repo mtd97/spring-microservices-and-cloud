2cfddd7121488e45b45e349987da1d00531b6dfe
mtd <minhtaiduc@gmail.com>
2024-03-06 02:21:11 +0700

    Initial commit

---
+++ photo-app-api-config-server/src/test/java/com/appsdeveloperblog/photoapp/api/PhotoAppApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapp.api;
+ 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+ 
+@SpringBootTest
+public class PhotoAppApiConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
